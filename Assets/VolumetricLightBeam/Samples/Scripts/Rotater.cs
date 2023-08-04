10486da08656702c00980168422091701c095932
“muddyrain” <qiu3291002845@gmail.com>
2023-08-04 10:42:59 +0800

    feat: init

---
+++ Assets/VolumetricLightBeam/Samples/Scripts/Rotater.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace VLB.Samples
+{
+    public class Rotater : MonoBehaviour
+    {
+        [FormerlySerializedAs("m_EulerSpeed")]
+        public Vector3 EulerSpeed = Vector3.zero;
+        public float Speed = 1;
+        void Update()
+        {
+            var euler = transform.rotation.eulerAngles;
+            euler += EulerSpeed * Time.deltaTime* Speed;
+            transform.rotation = Quaternion.Euler(euler);
+        }
+    }
+}
